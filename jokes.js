e9774c7e1f82da76da83adbb37c4129ca07a05c9
MarinusEIgenraam <rinuseigenraam@gmail.com>
2020-05-15 15:45:13 +0200

    funnyBot aired

---
+++ jokes.js
@@ -0,0 +1,38 @@
+// patients.js
+module.exports = {
+  nl: {
+      1: {
+          title: 'De bus',
+          setup: 'Er staat een man op de bus te wachten.',
+          punchline: 'Komt de buschaufeur naar buiten en roept: "hey kom eens van mijn bus af!',
+      },
+      2: {
+          title: 'De boom',
+          setup: 'Waarom kan een boom niet voetballen?',
+          punchline: 'Hij is geschorst!',
+      },
+      3: {
+          title: 'The Rus',
+          setup: 'In Rusland mogen er niet meer dan drie Russen in de auto.',
+          punchline: 'Anders krijg je virussen...',
+      },
+  },
+  en: {
+      1: {
+          title: 'The bus',
+          setup: 'There is a man waiting on the bus.',
+          punchline: 'Then the bus driver comes outside and yell\'s: "hey get off my bus!"',
+      },
+      2: {
+          title: 'The font',
+          setup: 'Helvetica and Times New Roman walk into a bar.',
+          punchline: '"Get out of here!" shouts the bartender. "We don\'t serve your type."',
+      },
+      3: {
+          title: 'The doctor',
+          setup: 'A man tells his doctor, “Doc, help me. I\’m addicted to Twitter!”',
+          punchline: 'The doctor replies, “Sorry, I don\’t follow you...”',
+      },
+  },
+
+}
